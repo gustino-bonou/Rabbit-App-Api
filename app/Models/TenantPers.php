3f27dcd3d05dd625531d4045623e40e4c345f155
Augustin Bonou <augustbonou@gmail.com>
2023-06-12 17:00:38 +0000

    installation Tenancy

---
+++ app/Models/TenantPers.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Database\Models\Tenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+
+
+class TenantPers extends Tenant implements TenantWithDatabase
+{
+    use HasDatabase, HasDomains;
+}
